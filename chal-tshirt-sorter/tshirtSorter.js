8630032aa4bc0d7167af4de0a26f1f6ce1e74f28
Lily Springer <lilyspringer@hotmail.com>
2023-03-17 21:18:23 -0600

    tshirt sorter done

--- chal-tshirt-sorter/tshirtSorter.js
+++ chal-tshirt-sorter/tshirtSorter.js
@@ -1,1 +1,21 @@
 // Write your solution below:
+
+function shirtSorter(str) {
+    let s = ''
+    let m = ''
+    let l = ''
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === 's') {
+            s += 's'
+        } else if (str[i] === 'm') {
+            m += 'm'
+        } else {
+            l += 'l'
+        }
+    }
+
+    return s + m + l
+}
+
+console.log(shirtSorter('ssmllmslmlmslml'))
