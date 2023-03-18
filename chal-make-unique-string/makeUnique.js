d3ea6e7f5b41901bcb60d959f4a06989d2284bb8
Lily Springer <lilyspringer@hotmail.com>
2023-03-17 20:44:26 -0600

    makeUnique done

--- chal-make-unique-string/makeUnique.js
+++ chal-make-unique-string/makeUnique.js
@@ -1,1 +1,16 @@
 // Write your solution below:
+
+function makeUnique (string) {
+    let solution = ""
+
+    for (let i = 0; i < string.length; i++) {
+        if (solution.includes(string[i])) {
+            continue
+        } else {
+            solution += string[i]
+        }
+    }
+    return solution
+}
+
+console.log(makeUnique('twasbrilligandtheslithytoves'))
